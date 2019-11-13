83671a0b24f80114c5d37e06c252ee86ea5c91c5
dylvne <delane4.0@gmail.com>
2019-11-13 13:12:05 -0500

    I fixed the bug. I wasn't thinking about how react loads it's components and restructured the Item page component.

--- client/src/ItemPage.js
+++ client/src/ItemPage.js
@@ -1,8 +1,6 @@
 import React from "react";
 import {useParams} from 'react-router-dom';
 
-let chosenItem = "";
-let ItemObj;
 
 function replaceDashes(x){
     var str = x;
@@ -10,57 +8,32 @@ function replaceDashes(x){
     return str;
 }
 
-function ItemTitle(){
+function ItemPage(props){
+    // constructor(props){
+    //     super(props);
+    // }
     let { Item } = useParams();
-    //console.log(replaceDashes(Item))
-    return(replaceDashes(Item))
-}
-
-class ItemPage extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {};
+    console.log(Item);
+    let itemObj;
+    for(let i = 0; i < props.storeData.length; i ++){
+        if(replaceDashes(Item) === props.storeData[i].sync_product.name.toLowerCase()){
+            itemObj = props.storeData[i]
+        }
     }
-
-    static getDerivedStateFromProps(nextProps, prevState){
-
-        // if (nextProps.userId !== prevState.prevUserId) {
-        //     return {
-        //       prevUserId: nextProps.userId,
-        //       profileOrError: null,
-        //     };
-        //   }
-      
-          // No state update necessary
-          return null;
-    }
+    console.log(itemObj)
     
-    render (){
-        let storeData = this.props.storeData;
-        console.log(storeData)
-        //let chosenItem = "";
-        //console.log(chosenItem)
-
+    
         return(
             <div>
-                <h1 id={"chosenItem"}><ItemTitle /></h1>
+                <h1>{replaceDashes(Item)}</h1>
+            <p>{JSON.stringify(itemObj)}</p>
             </div>
         )
-    }
+    
 
-    componentDidMount(){
-        
-        chosenItem =document.getElementById("chosenItem").innerHTML;
-        console.log(chosenItem)
+    
 
-        for(let i = 0; i <this.props.storeData.length; i ++){
-            if(chosenItem === this.props.storeData[i].sync_product.name.toLowerCase()){
-                ItemObj = this.props.storeData[i];
-                console.log(ItemObj);
-            }
-        }
-
-    }
+    
     
 }
 export default ItemPage;
