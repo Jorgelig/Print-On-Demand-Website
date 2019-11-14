07967e554483d82d14d4d91864e1a5b648e9df12
dylvne <delane4.0@gmail.com>
2019-11-13 19:07:57 -0500

    added a way to cycle through the different options

--- client/src/ItemPage.js
+++ client/src/ItemPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {useState} from "react";
 import {useParams} from 'react-router-dom';
 
 
@@ -15,6 +16,9 @@ function ItemPage(props){
     let { Item } = useParams();
     console.log(Item);
     let itemObj;
+    //let preview = 0;
+    const [count, setCount] = useState(0);
+    
     for(let i = 0; i < props.storeData.length; i ++){
         if(replaceDashes(Item) === props.storeData[i].sync_product.name.toLowerCase()){
             itemObj = props.storeData[i]
@@ -22,13 +26,39 @@ function ItemPage(props){
     }
     console.log(itemObj)
     
-    
+    if (itemObj){
         return(
-            <div>
-                <h1>{replaceDashes(Item)}</h1>
-            <p>{JSON.stringify(itemObj)}</p>
+            <div>                
+                <h1>{Item}</h1>
+                <img src={itemObj.sync_variants[count].files[1].preview_url} alt={itemObj.sync_variants[count].name} />
+                <p>{itemObj.sync_variants[count].name}</p>
+                <button onClick={
+                    function(){
+                        if(count > 0){
+                            setCount(count - 1);
+                            console.log(count);
+                        }
+                        else{console.log(count)}
+                    }
+                }>BACK</button>
+                <button onClick={
+                    function(){
+                        if(count < itemObj.sync_variants.length - 1){
+                            setCount(count + 1);
+                            console.log(count);
+                        }
+                        else{console.log(count)}
+                    }
+                }>NEXT</button>
             </div>
         )
+    }
+    else{
+        return(
+            <div>
+            </div>
+        )
+    }
     
 
     
